a10e03f00f292b0ef395e90c4dd4e4a69841b0b3
Finn Womack <flan313@gmail.com>
2016-08-03 12:18:25 -0700

    Changed genematrix to output a dataframe

--- framespacer/R/genematrix.R
+++ framespacer/R/genematrix.R
@@ -1,11 +1,15 @@
-#' Converts a dataframe/slice response into a matrix
-#' @description Converts a response from a dataframe/slice request into a matrix stuctured with genes as rows and samples as collumns. This allows you to call on data like so: your_data["genename", c("samplename1", "samplename2", "samplename3")].
+#' Converts a dataframe/slice response into a dataframe
+#' @description Converts a response from a dataframe/slice request into a dataframe or matrix stuctured with genes as collumns and samples as rows. This allows you to call on data like so: your_data[c("samplename1", "samplename2", "samplename3"), "genename"].
 #' @param resp \strong{list} A responce from a dataframe/slice request
+#' @param df \strong{bool} Leave TRUE if you want a dataframe or set to FALSE if you want a matrix
 #' @return A matrix structured with genes as rows and samples as collumns.
 #' @export
-genematrix <- function(resp){
+genematrix <- function(resp, df = TRUE){
   genes <- names(resp$contents)
   samples <- names(resp$contents[[genes[[1]]]])
-  mat <- matrix(unlist(resp$contents), ncol = length(samples), dimnames = list(genes, samples))
+  mat <- t(matrix(unlist(resp$contents), ncol = length(samples), dimnames = list(genes, samples)))
+  if (df){
+    mat <- as.data.frame(mat)
+  }
   return(mat)
 }
